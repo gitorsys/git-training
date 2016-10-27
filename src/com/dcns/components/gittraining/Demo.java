ba69971ba00b56da43ae001756d25fdcd6463cb1
Estelle Bremont <estelle.bremont@dcnsgroup.com>
2016-10-27 16:16:55 +0100

    Adding a dummy java file

---
+++ src/com/dcns/components/gittraining/Demo.java
@@ -0,0 +1,12 @@
+package com.dcnsgroup.gittraining;
+
+
+public class Demo {
+
+    /**
+    * Main method, which purpose is just to display a dummy log.
+    */
+    public static void main(String... args) {
+        System.out.println("Hello world");
+    }
+}
