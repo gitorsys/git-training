c0362f3925cd76b6980670321517a754d449e898
Alain BETBEDER <alain.betbeder@dcnsgroup.com>
2016-10-28 08:38:14 +0100

    close#5

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 function helloWorld(){
   //say hello to the word
-  System.out.println("Hello World");
+  console.log("Hello World");
 }
 
 function helloKitty(){
   //say hello to kitty
-  System.out.println("Hello Kitty);
+  console.log("Hello Kitty);
 }
 
 function helloFrance(){
-  //say hello to france
-  System.out.println("Hello France");
+  //say hello to France
+  console.log("Hello France");
 
 }
 
